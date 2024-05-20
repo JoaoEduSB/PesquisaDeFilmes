dd5cf2041ed1da6c159ed8d3673edc40e60762d1
João Eduardo <joao.eduardo.souza@hotmail.com>
2024-05-20 18:19:39 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import BuscaDeFilmes from './Components/BuscaDeFilmes'
-import './App.css'
+import BuscaDeFilmes from './Components/BuscaDeFilmes.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
